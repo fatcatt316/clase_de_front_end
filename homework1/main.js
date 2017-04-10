b8cd60fc1bad90172fc86c8b6ef9f2f97e4f405b
Joe Peck <fatcatt316@yahoo.com>
2017-04-09 22:06:06 -0400

    Sound FX

--- homework1/main.js
+++ homework1/main.js
@@ -2,3 +2,11 @@
 // play noise when shoots
 // shoot laser
 // smoke and fire
+
+$(document).ready(function() {
+  var shotty = $('#shotty');
+
+  $(window).click(function(e) {
+    shotty[0].play();
+  });
+});
