fb170435c9a80c05e81e065e2c73918fe56cea69
Joe Peck <fatcatt316@yahoo.com>
2017-05-18 21:52:28 -0400

    Get AJAX GET working; add loading picture

--- homework3/yavascript.js
+++ homework3/yavascript.js
@@ -1,23 +1,16 @@
 $(function() {
 
-  // TODO: Put it inside an IIFE or one of them namespaced things
   $('.ajax-it-up').on('click', function(event) {
-    var _this = this;
+    $('#resultz').html("<img src='loading_cat.gif'>");
 
-    $.ajax({
-      url: 'http://swapi.co/api/people/1',
-      method: 'GET',
-    }).then(function(data) {
-      console.log(data);
-      $('#resultz').html()
-    }).catch(function(err) {
-      // try bad endpoint, see an error
-      console.log('we got an error!');
-      console.log(err);
+    $.get({
+      url: 'http://thecatapi.com/api/images/get.php?format=html&type=gif',
+      dataType: 'html'
+    }).always(function(data) {
+      // Every response has an XML parsing error, so I'm using "always".
+      // I think the API is messed up, but I couldn't find another cat gif one :(
+      $('#resultz').html(data);
     });
   });
 
 });
-
-
-// https://www.programmableweb.com/category/humor/apis?category=19997
