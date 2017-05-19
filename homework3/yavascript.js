d381558740f1a2b71fd1e3d2423c45d1ceb2b4f7
Joe Peck <fatcatt316@yahoo.com>
2017-05-18 22:22:38 -0400

    Disable button when it's pressed

--- homework3/yavascript.js
+++ homework3/yavascript.js
@@ -1,7 +1,12 @@
 $(function() {
 
-  $('.ajax-it-up').on('click', function(event) {
+  var initializeRequest = function(button) {
     $('#resultz').html("<img src='loading_cat.gif'>");
+    button.prop('disabled', true);
+  }
+
+  $('.ajax-it-up').on('click', function(event) {
+    initializeRequest($(event.target));
 
     $.get({
       url: 'http://thecatapi.com/api/images/get.php?format=html&type=gif',
@@ -10,6 +15,7 @@ $(function() {
       // Every response has an XML parsing error, so I'm using "always".
       // I think the API is messed up, but I couldn't find another cat gif one :(
       $('#resultz').html(data);
+      $(event.target).prop('disabled', false);
     });
   });
 
