bbbcaef7c4f9cf051b0f8d8a569ed074e7ddbc7f
Joe Peck <fatcatt316@yahoo.com>
2017-07-08 13:23:44 -0400

    Revert "Comment out Google stuff, maybe final commit?"
    This reverts commit 03998e27e15d153cec0a09cdce06904f0f4d662d.

--- final_project/server/index.js
+++ final_project/server/index.js
@@ -4,13 +4,10 @@ const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('../config/webpack.config.js');
-// const rp = require('request-promise');
-// const bodyParser = require('body-parser'); // for reading JSON in requests.
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 const app = express();
-// app.use(bodyParser.json());
 
 if (isDeveloping) {
   const compiler = webpack(config);
@@ -32,43 +29,26 @@ if (isDeveloping) {
   app.get('/requirements', (req, res) => {
     res.sendFile(path.join(__dirname, '../app/requirements.html'));
   });
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '/../dist/index.html')));
     res.end();
   });
 } else {
   app.use(express.static(path.join(__dirname, '/../dist')));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 }
 
-// app.get('/google-results', function(req, res) {
-//   let googleUrl;
-//   console.log("Shucks");
-
-//   const {query: {city, name}} = req;
-//   console.log("CITY: ", city);
-//   console.log("NAME: ", name);
-
-  // if (req.query.name) {
-  //   googleUrl = 'https://www.google.com/search?q="' + encodeURI('"'+ req.query.name + '"');
-  //   if (req.query.city) {
-  //     googleUrl = googleUrl + '+' + encodeURI(req.query.city);
-  //   }
-  // } else {
-    return 'NO RESULTS';
-  // }
-
+// app.post('/api/our_custom_endpoint', function(req, res) {
+//   var url = `http://numbersapi.com/${req.body.number}`
 //   var options = {
 //     method: 'GET',
-//     uri: googleUrl
+//     uri: url
 //   };
 //   // Hit API using request-promise library
 //   rp(options)
 //     .then(function(resp) {
-//       console.log("HERE WE GO");
-//       console.log(resp);
 //       res.send({
 //         resp: resp
 //       });
